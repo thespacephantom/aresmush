7ec4b77fd049369d241f9c5298b3bcf9242caf57
Astra <adler.orr@gmail.com>
2021-01-28 18:26:27 -0600

    Update set_territory_cmd.rb

--- plugins/territory/commands/set_territory_cmd.rb
+++ plugins/territory/commands/set_territory_cmd.rb
@@ -3,14 +3,14 @@ module AresMUSH
     class SetTerritoryCmd
       include CommandHandler
 
-      attr_accessor :territory
+      attr_accessor :territory_faction
 
       def parse_args
        self.territory = trim_arg(cmd.args)
       end
 
       def handle
-        enactor_room.update(territory: self.territory_faction)
+        enactor_room.update(territory_faction: self.territory)
         client.emit_success "Territory for this room set!"
       end
     end
