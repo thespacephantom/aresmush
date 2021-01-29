ba82b26f4dc5f826e09bd03681415ed1df21b9ef
Astra <adler.orr@gmail.com>
2021-01-28 18:33:54 -0600

    Update set_territory_cmd.rb

--- plugins/territory/commands/set_territory_cmd.rb
+++ plugins/territory/commands/set_territory_cmd.rb
@@ -9,6 +9,11 @@ module AresMUSH
        self.territory = trim_arg(cmd.args)
       end
 
+      def check_can_view
+          return nil if enactor.has_permission?("set_territory")
+          return "You're not allowed to set territories."
+      end
+
       def handle
         enactor_room.update(territory: self.territory)
         client.emit_success "Territory for this room set!"
